7657ac90b13ab3b37600eb303f4549f9ecdb0331
guna398 <106138952+guna398@users.noreply.github.com>
2022-08-23 10:46:21 +0530

    Add files via upload

---
+++ mathematical.js
@@ -0,0 +1,9 @@
+const a = 800;
+const b = 1000;
+console.log(`the value of A = ${a}`);
+console.log(`the value of B = ${b}`);
+console.log("Mathematical Expression....");
+console.log("addition of A and B is ",(a+b));
+console.log("subraction of A and B is ",(a-b));
+console.log("multiplication of A and B is ",(a*b));
+console.log("division of A and B is ",(a/b));
