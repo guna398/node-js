9e7f90281da3bfa924af81ff0ccbdd67162a6c0f
Guna Palani <106138952+gunapalani-3998@users.noreply.github.com>
2022-09-09 12:21:37 +0530

    Add files via upload

---
+++ string.js
@@ -0,0 +1,12 @@
+let s="Welcome to 2022";
+console.log(s.length);                                // to find length
+console.log(s.indexOf("e"));                          // index position of first string
+console.log(s.replace(2022,"Indium"));                // to replace the char
+console.log(s.substring(3,6));                         // get a range b/w start to end 
+console.log(s.split(""));                              // string to array
+console.log(s.substr(3,4));                         // no of char from starting point
+console.log(s.charAt(3));                              //char based on index pos
+console.log(s.toUpperCase(""));                     //upper case str
+console.log(s.charCodeAt(3));                       // ASCII value (american standard code for information intercharge)
+
+//Str 
