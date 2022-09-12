cfe49ebde945c6989e74a9cd38df52c0aa9de4ac
Guna Palani <106138952+gunapalani-3998@users.noreply.github.com>
2022-09-12 13:42:18 +0530

    Add files via upload
    Looping func

---
+++ loop.js
@@ -0,0 +1,9 @@
+// loop function if value greater than 10 we use the keyword BREAK to stop
+
+    for (let j = 1; ; j += 2) {
+        console.log(j);
+            if (j > 10) 
+            {
+                break;
+            }
+     }
