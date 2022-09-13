d0145e837d3b5fea4161fa849723aea483039843
Guna Palani <106138952+gunapalani-3998@users.noreply.github.com>
2022-09-13 12:42:14 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,14 @@
+let a = ["Guna","Bachelor of Engineering","guna@gmail.com",'9843250625'];
+let b = ["Chennai","Indium"];
+console.log(a.sort());
+console.log(a.reverse());
+console.log(a.concat(b));
+console.log(a.length);
+console.log(a.join("-"));
+a.pop();
+a.shift();
+console.log(a);
+a.push("India");
+console.log(a.slice(2,4));
+console.log(a.splice(3,0,"male"));
+console.log(a);
