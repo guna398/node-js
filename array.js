d6e90783014c3cdceb4c53b08f9628a168c0314e
Guna Palani <106138952+gunapalani-3998@users.noreply.github.com>
2022-10-26 13:22:01 +0530

    array and reverse, sorting worked

--- array.js
+++ array.js
@@ -1,18 +1,10 @@
-
-// Array pre-def function
-// Syntax : let objname=[a1,a2...];
-
-let a = ["Guna","Bachelor of Engineering","guna@gmail.com",'9843250625'];
-let b = ["Chennai","Indium"];
-console.log(a.sort());
-console.log(a.reverse());
-console.log(a.concat(b));
-console.log(a.length);
-console.log(a.join("-"));
-a.pop();
-a.shift();
-console.log(a);
-a.push("India");
-console.log(a.slice(2,4));
-console.log(a.splice(3,0,"male"));
-console.log(a);
+let numbers = [1,2,3,4];
+let number = 5;
+const addElement = (...args)=>{
+    let result = [];
+    for (const iterator of args) {
+        result.push(iterator);
+    }
+    return result;
+}
+console.log(addElement(...numbers,number));
